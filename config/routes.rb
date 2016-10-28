7ba079316fb39da64b49e1482f96c36471aa6194
taka yaeo <taka-yaeo@yaeo.local>
2016-10-28 19:20:54 +0900

    Create users controller and edit action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'stories#index'
-  resources :stories, only:[:show]
+  resources :stories, only: [:show]
+  get "profile", :to => 'users#edit'
+  resource :users, path: 'profile', as: 'profile', only: [:update]
 end
