483e5959fd06b9db062e3dd17778d4620fc83dff
taka yaeo <taka-yaeo@yaeo.local>
2016-10-31 12:48:29 +0900

    Use 'profile' for edit page url

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'stories#index'
   resources :stories, only: [:show]
-  resources :users, only: [:edit, :update]
+  get "profile", :to => 'users#edit'
+  resources :users, only: [:update]
 end
