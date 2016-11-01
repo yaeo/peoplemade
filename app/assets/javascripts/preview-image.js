206ccded7c58d0132ba15f24037bae3da0b537da
taka yaeo <taka-yaeo@yaeo.local>
2016-11-01 20:42:07 +0900

    Show image before upload

---
+++ app/assets/javascripts/preview-image.js
@@ -0,0 +1,23 @@
+$(function(){
+  var reader = new FileReader();
+  // inputに変化があったらスタート
+  $('.image-selector').on('change', function() {
+    // 変化があったもののidを取得
+    var id = this.getAttribute("id");
+    // filesの0番目を読み込む
+    reader.readAsDataURL(this.files[0]);
+    // 読み込みが終わったらスタート
+    reader.addEventListener('load', function(){
+      // idに応じて画像を差し替え
+      if (id == "story_topics_attributes_0_image") {
+        $('#1-image').attr('src', reader.result);
+      } else if (id == "story_topics_attributes_1_image") {
+        $('#2-image').attr('src', reader.result);
+      } else if (id == "story_topics_attributes_2_image") {
+        $('#3-image').attr('src', reader.result);
+      } else if (id == "story_topics_attributes_3_image") {
+        $('#4-image').attr('src', reader.result);
+      }
+    });
+  });
+});
