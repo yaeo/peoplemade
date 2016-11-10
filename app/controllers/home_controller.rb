52b5baa6805750da6cdcd91679541ef5f50ad808
taka yaeo <taka-yaeo@yaeo.local>
2016-11-10 20:24:40 +0900

    Show latest stories for root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @list_stories = Story.published
+    @list_stories = Story.published.order("created_at DESC")
     @recommends_stories = Story.published.limit(3).order("updated_at DESC")
   end
 end
