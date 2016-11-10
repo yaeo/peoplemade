b9470eff3a6f7883a849fec2c6fa15491be8585b
taka yaeo <taka-yaeo@yaeo.local>
2016-11-10 18:55:43 +0900

    Replace regulation-image when user uploads story.image

--- app/assets/javascripts/edit_topic.js
+++ app/assets/javascripts/edit_topic.js
@@ -9,16 +9,7 @@ $(document).on('turbolinks:load', function() {
       reader.readAsDataURL(this.files[0]);
       // 読み込みが終わったらスタート
       reader.addEventListener('load', function(){
-        // idに応じて画像を読み込み結果に差し替え
-        if (id == "story_topics_attributes_0_image") {
-          $('#1-image').attr('src', reader.result);
-        } else if (id == "story_topics_attributes_1_image") {
-          $('#2-image').attr('src', reader.result);
-        } else if (id == "story_topics_attributes_2_image") {
-          $('#3-image').attr('src', reader.result);
-        } else if (id == "story_topics_attributes_3_image") {
-          $('#4-image').attr('src', reader.result);
-        }
+        $('#regulation-image').attr('src', reader.result);
       });
     });
   });
