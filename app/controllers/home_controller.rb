3844f0b20f6fdd8f97325c0434658c16db2d022b
taka yaeo <taka-yaeo@yaeo.local>
2016-11-10 20:23:37 +0900

    Show latest stories for recommend stories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @list_stories = Story.published
+    @recommends_stories = Story.published.limit(3).order("updated_at DESC")
   end
 end
