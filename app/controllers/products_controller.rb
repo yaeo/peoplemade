6f51d9c71ed3ebfada371b7dd4f812f748321dbf
taka yaeo <taka-yaeo@yaeo.local>
2016-11-14 19:29:51 +0900

    Add image to strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def new
     @product = Product.new
+    @product.images.build
   end
 
   def create
@@ -12,6 +13,7 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :company_id)
+      params.require(:product).permit(:name, :description, :price, :company_id, :url, 
+        images_attributes: [:image])
     end
 end
