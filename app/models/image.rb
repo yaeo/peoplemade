722d306f4fb394f599871c87a3b19707cf74dc35
taka yaeo <taka-yaeo@yaeo.local>
2016-11-14 19:13:19 +0900

    Add association to image and product model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ApplicationRecord
+  #association
+  belongs_to :product
+
+  mount_uploader :image, ImageUploader
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   #association
-  belongs_to :company
+  belongs_to  :company
+  has_many    :images
+
+  accepts_nested_attributes_for :images
 end
