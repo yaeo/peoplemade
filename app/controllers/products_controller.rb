6bd2d16a2b9d9dc92585699d966e9e8c414bf5ac
taka yaeo <taka-yaeo@yaeo.local>
2016-11-14 19:53:53 +0900

    Upload multiple images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :company_id, :url, 
-        images_attributes: [:image])
+      params.require(:product).permit(:name, :description, :price, :company_id, :url,
+                                      :images_attributes => [:image])
     end
 end
