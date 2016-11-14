14fe8eac36167f1ec14912b1514f855d56cdb77b
taka yaeo <taka-yaeo@yaeo.local>
2016-11-14 18:34:38 +0900

    Use strong params to create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,6 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :description, :price)
+      params.require(:product).permit(:name, :description, :price, :company_id)
     end
 end
