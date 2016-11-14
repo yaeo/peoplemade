d95c5245b4a2c5cb65b2bf681afb0470fff2b8bc
taka yaeo <taka-yaeo@yaeo.local>
2016-11-14 18:26:21 +0900

    Create "create" action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    product = Product.new(product_params)
+    product.save
+    redirect_to root_url
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
 end
