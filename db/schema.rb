ab0cdd24a4fc9b95fcb819276d66d7e85437f030
taka yaeo <taka-yaeo@yaeo.local>
2016-11-14 18:12:56 +0900

    Create product model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110064435) do
+ActiveRecord::Schema.define(version: 20161114062744) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 20161110064435) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.index ["user_id"], name: "index_companies_on_user_id", using: :btree
+  end
+
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "company_id"
+    t.string   "name"
+    t.integer  "price"
+    t.text     "description", limit: 65535
+    t.string   "url"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["company_id"], name: "index_products_on_company_id", using: :btree
   end
 
   create_table "socials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
