8b45d3b2c9e49c359fea89a5d4ebc86e97608ef5
taka yaeo <taka-yaeo@yaeo.local>
2016-11-17 20:04:42 +0900

    Able to edit products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,19 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
+  end
+
   private
 
     def product_params
