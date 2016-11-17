c306fc8d9d1939c7ebba7f7fd49e4549cd689222
taka yaeo <taka-yaeo@yaeo.local>
2016-11-17 19:36:08 +0900

    Show company's products in company detail page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,5 +2,6 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find_by(custom_url: params[:custom_url])
     @company_stories = @company.user.stories.published
+    @products = @company.products
   end
 end
