9d1137d0019c80843d7d97a1217934e75cd4c697
taka yaeo <taka-yaeo@yaeo.local>
2016-11-21 21:00:51 +0900

    Create is_company_nil? method to seek current_user's company record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def is_company_nil?
+    if current_user.company.nil?
+      @msg = "hello world"
+      redirect_to profile_url, notice: "🙇 ストーリー/商品の作成前に基本情報のご登録をお願いいたします 🙇"
+    end
+  end
 end
