4af2f69d57de12b17282b48ebf367fa8cff0ed25
taka yaeo <taka-yaeo@yaeo.local>
2016-11-22 20:45:16 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'home#index'
   resources :stories
+  resources :products
   get "profile", :to => 'users#edit'
   resources :users, only: [:update]
-  resources :products
   get ':custom_url', :to => 'companies#show', as: 'company'
   resources :uploads, only: [:create]
 
