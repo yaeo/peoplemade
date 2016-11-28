c77de705dce0590fc271e96f3aab6b9f1730658f
taka yaeo <taka.yaeo@gmail.com>
2016-11-28 13:39:14 +0900

    Show recommend stories randomly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
   def index
-    #ピックアップストーリー用に最新と次最新のストーリーを取得
+    #メインピックアップストーリー用に最新と次最新のストーリーを取得
     @pickup_stories = Story.published.order("updated_at DESC").limit(2)
     #ストーリーの一覧表示用に3番目に新しいストーリーから取得
     @stories = Story.published.order("updated_at DESC")[2..-1]
+    #商品の一覧表示用に全商品を取得
     @product_lists = Product.all
-    @recommend_stories = Story.published.limit(3).order("updated_at DESC")
+    #注目ストーリー用に3つストーリーを取得
+    @recommend_stories = Story.published.order("RAND()").limit(3)
     #ストーリーを掲載した企業を取得する
     unique_story_ids = Story.select("user_id").distinct
     user_ids = unique_story_ids.map(&:user_id)
