c58356f1c20fcb542126376dcbcf97f5ed64df6b
taka yaeo <taka.yaeo@gmail.com>
2016-11-28 13:09:15 +0900

    Render story partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @list_stories = Story.published.order("created_at DESC")
+    @stories = Story.published.order("created_at DESC")
     @product_lists = Product.all
     @recommend_stories = Story.published.limit(3).order("updated_at DESC")
     unique_story_ids = Story.select("user_id").uniq
