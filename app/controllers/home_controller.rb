143868a06ae72a2a52f1872d43acdce59ed33b80
taka yaeo <taka.yaeo@gmail.com>
2016-11-28 13:23:00 +0900

    Get newest stories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @stories = Story.published.order("created_at DESC")
+    #最新と次最新のストーリーを取得
+    @pickup_stories = Story.published.order("updated_at DESC").limit(2)
+    #3番目に新しいストーリーから取得
+    @stories = Story.published.order("updated_at DESC")[2..-1]
     @product_lists = Product.all
     @recommend_stories = Story.published.limit(3).order("updated_at DESC")
     unique_story_ids = Story.select("user_id").uniq
     user_ids = unique_story_ids.map(&:user_id)
     @company_lists = Company.where(user_id: user_ids)
-    @pickup_stories = Story.published.order("updated_at DESC").limit(2)
+
   end
 end
