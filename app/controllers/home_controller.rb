d31030323631d42828e645a9f23feb52d25268fe
taka yaeo <taka.yaeo@gmail.com>
2016-11-28 13:29:53 +0900

    Use 'distinct' instead of 'uniq'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
   def index
-    #最新と次最新のストーリーを取得
+    #ピックアップストーリー用に最新と次最新のストーリーを取得
     @pickup_stories = Story.published.order("updated_at DESC").limit(2)
-    #3番目に新しいストーリーから取得
+    #ストーリーの一覧表示用に3番目に新しいストーリーから取得
     @stories = Story.published.order("updated_at DESC")[2..-1]
     @product_lists = Product.all
     @recommend_stories = Story.published.limit(3).order("updated_at DESC")
-    unique_story_ids = Story.select("user_id").uniq
+    #ストーリーを掲載した企業を取得する
+    unique_story_ids = Story.select("user_id").distinct
     user_ids = unique_story_ids.map(&:user_id)
     @company_lists = Company.where(user_id: user_ids)
-
   end
 end
