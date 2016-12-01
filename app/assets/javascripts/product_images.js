c4f04770b33c0d3b64b39c59bdb98e412649f4c5
taka yaeo <taka.yaeo@gmail.com>
2016-12-01 18:07:27 +0900

    Remove unnecessary codes

--- app/assets/javascripts/product_images.js
+++ app/assets/javascripts/product_images.js
@@ -13,18 +13,4 @@ $(document).on('turbolinks:load', function() {
       }, obj);
     });
   });
-
-  $(function(){
-    $('#product_images_attributes_0_images').on('change', function() {
-      var obj = this.files;
-      Object.keys(obj).forEach(function(key) {
-        var reader = new FileReader();
-        var val = this[key];
-        reader.readAsDataURL(val);
-        reader.addEventListener('load', function(){
-          $('#selected-images').append('<li><img src ='+reader.result+'></li>');
-        })
-      }, obj);
-    });
-  });
 });
