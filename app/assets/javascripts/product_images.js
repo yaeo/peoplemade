ba9ad35f246d5fa394291f447a87718281acefb5
taka yaeo <taka.yaeo@gmail.com>
2016-12-01 14:32:10 +0900

    Create js file for showing selected images

---
+++ app/assets/javascripts/product_images.js
@@ -0,0 +1,16 @@
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    // inputに変化があったらスタート
+    $('#product_images_attributes_0_image').on('change', function() {
+      var obj = this.files;
+      Object.keys(obj).forEach(function(key) {
+        var reader = new FileReader();
+        var val = this[key];
+        reader.readAsDataURL(val);
+        reader.addEventListener('load', function(){
+          $('#selected-images').append('<li><img src ='+reader.result+'></li>');
+        })
+      }, obj);
+    });
+  });
+});
